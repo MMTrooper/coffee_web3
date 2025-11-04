28c5bc8fc42edc1c3ea238fb430dd0958d42c11d
Michael Mwimali <8977534+YourUsername@users.noreply.github.com>
2025-11-04 17:45:24 +0300

    Created a fresh coffee website based on svelte

---
+++ coffee_svelte2/src/components/SectionWrapper.svelte
@@ -0,0 +1,10 @@
+<!-- Used to keep style consistency accross components -->
+<script>
+    export let id;
+</script>
+
+<section {id} class={"min-h-screen flex flex-col px-4 "}>
+    <div class="flex flex-col flex-1 max-w-[1400px] mx-auto w-full">
+        <slot />
+    </div>
+</section>
---
+++ coffee_svelte2/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
+
+<Footer />
+
+<!-- header
+hero
+product description
+mission in place of reviews
+services in place of faq
+contact in place of conversion
+footer -->
